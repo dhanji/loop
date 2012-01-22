0e7d3b56cdb137ba932d28365b5959aafc903a81
Dhanji R. Prasanna <dhanji@gmail.com>
2012-01-22 16:10:57 +1100

    Support for invoking Java Literals and constants #2

--- test/loop/confidence/interop/JavaInteropConfidenceTest.java
+++ test/loop/confidence/interop/JavaInteropConfidenceTest.java
@@ -3,6 +3,8 @@ package loop.confidence.interop;
 import loop.Loop;
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -13,6 +15,8 @@ import static org.junit.Assert.assertTrue;
  * @author dhanji@gmail.com (Dhanji R. Prasanna)
  */
 public class JavaInteropConfidenceTest {
+  public static final Long CONSTANT = new Date().getTime();
+
   @Test
   public final void normalFunctionCall() {
     assertEquals("hello", Loop.run("test/loop/confidence/interop/postfix_call_1.loop"));
@@ -22,5 +26,10 @@ public class JavaInteropConfidenceTest {
   public final void callJavaStaticFunction() {
     assertTrue(1327205727145L /* epoch time when this test was written */ < Long.parseLong(
         Loop.run("test/loop/confidence/interop/call_java_static.loop").toString()));
+  }
+
+  @Test
+  public final void callJavaStaticConstant() {
+    assertEquals(CONSTANT + 1, Loop.run("test/loop/confidence/interop/call_java_static_2.loop"));
   }
 }
