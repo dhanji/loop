ad943c223a9cbabfc3da125369267baf4ee5a90f
Dhanji R. Prasanna <dhanji@gmail.com>
2012-02-09 23:08:50 +1100

    Prevent mutation of immutable loop objects through view sets

--- src/loop/lang/ImmutableLoopObject.java
+++ src/loop/lang/ImmutableLoopObject.java
@@ -2,7 +2,10 @@ package loop.lang;
 
 import loop.LoopExecutionException;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * The root object of all immutable object instances in loop. Actually this is the
@@ -10,7 +13,7 @@ import java.util.Map;
  *
  * @author dhanji@gmail.com (Dhanji R. Prasanna)
  */
-public class ImmutableLoopObject extends LoopObject{
+public class ImmutableLoopObject extends LoopObject {
   private static final String IMMUTABILITY_ERROR =
       "Illegal attempt to create an object oriented language!";
 
@@ -32,5 +35,17 @@ public class ImmutableLoopObject extends LoopObject{
 
   @Override public Object remove(Object o) {
     throw new LoopExecutionException(IMMUTABILITY_ERROR);
+  }
+
+  @Override public Set<Object> keySet() {
+    return Collections.unmodifiableSet(super.keySet());
+  }
+
+  @Override public Collection<Object> values() {
+    return Collections.unmodifiableCollection(super.values());
+  }
+
+  @Override public Set<Map.Entry<Object, Object>> entrySet() {
+    return Collections.unmodifiableSet(super.entrySet());
   }
 }
