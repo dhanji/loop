9cc9e5ce1523d77160e11db9eccb8224d659371e
Dhanji R. Prasanna <dhanji@gmail.com>
2012-05-29 16:21:00 +1000

    Added license, readme and first distro

---
+++ release.sh
@@ -0,0 +1,24 @@
+
+if [ "$1" = "" ]; then
+  echo Please provide a commit message. e.g.:
+  echo ./release.sh \'Version 1.0\'
+  echo
+  exit
+fi
+
+TMP=build_tmp/loop
+
+mvn -o clean package assembly:single
+mkdir -p $TMP
+cp ./target/loop-*-jar-with-dependencies.jar ./$TMP/loop.jar
+cp loop $TMP/
+cp README $TMP/
+cp LICENSE $TMP/
+pushd build_tmp
+tar zcf ../dist/loop.tar.gz loop/*
+popd
+rm -rf build_tmp
+
+git add dist
+git commit -a -m "$1"
+git push
