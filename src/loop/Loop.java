a2360fe4edd105673696563c4cd26289e1574210
Dhanji R. Prasanna <dhanji@gmail.com>
2012-07-21 11:27:33 -0700

    Cleaned up the error message for missing loop files

--- src/loop/Loop.java
+++ src/loop/Loop.java
@@ -17,20 +17,26 @@ public class Loop {
       LoopShell.shell();
     }
 
-    if (args.length > 1)
-      run(args[0], args);
-    else
-      run(args[0]);
+    try {
+      if (args.length > 1)
+        run(args[0], args);
+      else
+        run(args[0]);
+    } catch (FileNotFoundException e) {
+      System.out.println("No such file: " + e.getMessage());
+      System.out.println();
+      System.exit(1);
+    }
   }
 
-  public static Object run(String file) {
+  public static Object run(String file) throws FileNotFoundException {
     Executable unit = loopCompile(file);
     unit.runMain(true);
 
     return safeEval(unit, null);
   }
 
-  public static Object run(String file, String[] args) {
+  public static Object run(String file, String[] args) throws FileNotFoundException {
     Executable unit = loopCompile(file);
     unit.runMain(true);
 
@@ -68,7 +74,11 @@ public class Loop {
    * Compiles the specified file into a binary Java executable.
    */
   public static Class<?> compile(String file) {
-    return loopCompile(file).getCompiled();
+    try {
+      return loopCompile(file).getCompiled();
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
@@ -76,19 +86,15 @@ public class Loop {
    * <p/>
    * See {@link Executable} for more details on the compilation process.
    */
-  private static Executable loopCompile(String file) {
+  private static Executable loopCompile(String file) throws FileNotFoundException {
     Executable executable;
-    try {
-      File script = new File(file);
-      executable = new Executable(new FileReader(script), script.getName());
-      executable.compile();
-      if (executable.hasErrors()) {
-        String errors = executable.printStaticErrorsIfNecessary();
+    File script = new File(file);
+    executable = new Executable(new FileReader(script), script.getName());
+    executable.compile();
+    if (executable.hasErrors()) {
+      String errors = executable.printStaticErrorsIfNecessary();
 
-        throw new LoopCompileException("Syntax errors exist:\n" + errors, executable);
-      }
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+      throw new LoopCompileException("Syntax errors exist:\n" + errors, executable);
     }
     return executable;
   }
