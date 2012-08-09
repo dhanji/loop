5f8296d68ddbbb14664c208301ee84f090d0c0e4
Dhanji R. Prasanna <dhanji@gmail.com>
2012-08-09 10:41:11 +1000

    Allow loop programs to be executed from a given Reader

--- src/loop/Loop.java
+++ src/loop/Loop.java
@@ -3,10 +3,7 @@ package loop;
 import loop.ast.script.Unit;
 import loop.runtime.LoopInvocationHandler;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.StringReader;
+import java.io.*;
 import java.lang.reflect.Proxy;
 
 /**
@@ -34,6 +31,10 @@ public class Loop {
     }
   }
 
+  /**
+   * Executes a loop program stored in {@code file} and returns the
+   * result of evaluating main()--which is optional.
+   */
   public static Object run(String file) {
     Executable unit = loopCompile(file);
     unit.runMain(true);
@@ -41,8 +42,25 @@ public class Loop {
     return safeEval(unit, null);
   }
 
+  /**
+   * Executes a loop program stored in {@code file} and returns the
+   * result of evaluating main()--which is optional. Arguments can
+   * be passed in which are sent to main() representing the command line.
+   */
   public static Object run(String file, String[] args) {
     Executable unit = loopCompile(file);
+    unit.runMain(true);
+
+    return safeEval(unit, args);
+  }
+
+  /**
+   * Executes a loop program read from {@code reader }and returns the
+   * result of evaluating main()--which is optional. Arguments can
+   * be passed in which are sent to main() representing the command line.
+   */
+  public static Object run(String name, Reader reader, String[] args) {
+    Executable unit = loopCompile(name, reader);
     unit.runMain(true);
 
     return safeEval(unit, args);
@@ -83,18 +101,29 @@ public class Loop {
   }
 
   /**
+   * Compiles the given script (fed by reader) into a binary Java executable.
+   */
+  public static Class<?> compile(String name, Reader reader) {
+    return loopCompile(name, reader).getCompiled();
+  }
+
+  /**
    * Returns an executable that represents the compiled form of the Loop program.
    * <p/>
    * See {@link Executable} for more details on the compilation process.
    */
   private static Executable loopCompile(String file) {
-    Executable executable;
     File script = new File(file);
+
     try {
-      executable = new Executable(new FileReader(script), script.getName());
+      return loopCompile(script.getName(), new FileReader(script));
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private static Executable loopCompile(String name, Reader reader) {
+    Executable executable = new Executable(reader, name);
     executable.compile();
     if (executable.hasErrors()) {
       String errors = executable.printStaticErrorsIfNecessary();
@@ -113,7 +142,7 @@ public class Loop {
    * is backed by the specified Loop module. The generated Java class is loaded into
    * the common runtime Loop class loader. See {@link LoopClassLoader} for details.
    *
-   * @param iface A Java interface that you wish to implement using Loop
+   * @param iface  A Java interface that you wish to implement using Loop
    * @param module The name of a Loop module minus the '.loop' extension. This name may
    *               contain a path-prefix from the current directory.
    */
@@ -123,7 +152,7 @@ public class Loop {
       throw new RuntimeException(iface + " is not an interface ");
     }
 
-    return (I) Proxy.newProxyInstance(LoopClassLoader.CLASS_LOADER, new Class[]{ iface },
+    return (I) Proxy.newProxyInstance(LoopClassLoader.CLASS_LOADER, new Class[]{iface},
         new LoopInvocationHandler(iface, module));
   }
 }
