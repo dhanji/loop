410abb70f3373f73e56823b7e1b97f382c28e243
Dhanji R. Prasanna <dhanji@gmail.com>
2012-08-18 16:06:26 +1000

    Adding :: as a restricted keyword rather than a lexer token

--- src/loop/RestrictedKeywords.java
+++ src/loop/RestrictedKeywords.java
@@ -1,5 +1,7 @@
 package loop;
 
+import java.util.List;
+
 /**
  * @author dhanji@gmail.com (Dhanji R. Prasanna)
  */
@@ -7,4 +9,14 @@ public class RestrictedKeywords {
   public static final String EXCEPT = "except";
   public static final String AS = "as";
   public static final String ENSURE = "@ensure";
+
+  public static boolean isStaticOperator(List<Token> tokens) {
+    if (null == tokens || tokens.size() != 2)
+      return false;
+    for (Token token : tokens) {
+      if (!":".equals(token.value))
+        return false;
+    }
+    return true;
+  }
 }
